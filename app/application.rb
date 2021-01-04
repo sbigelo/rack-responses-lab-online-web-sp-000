68c0be6f3303739618722b127ff3a35da921ac77
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 21:50:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   def call(env)
+<<<<<<< HEAD
     resp = Rack::Response.new
     now = Time.now
     if now.hour >= 12
@@ -13,3 +14,19 @@ class Application
   end
 
 end
+=======
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+  end
+
+  def pretty_response
+    (Time.now.to_i < 12:00) ?  ["<em>Good Morning!</em>"] : ["<strong>Good Afternoon!</strong>"]
+  end
+end
+
+# 
+# def call(env)
+#   resp = Rack::Response.new
+#   resp.write "Hello, World"
+#   resp.finish
+# end
+>>>>>>> cce6dd983328f3ea0130107891550caba043216e
